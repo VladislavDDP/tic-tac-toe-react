0ef18e85f3afd21ac7f9334b554e26b6495dea4f
Vladislav <bombardire54@gmail.com>
2021-09-26 23:12:22 +0300

    Local state to board and square

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,33 +1,43 @@
 import Square from "./Square"
 import './App.css';
+import React from "react";
 
-const Board = () => {
-    const status = 'Next player: X'
-
-    const renderSquare = (i) => {
-        return <Square number={i} />
+class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.status = 'Next player: X'
+        this.state = {
+            squares: Array(9).fill(null)
+        }
     }
+    
+    
+    renderSquare(i) {
+        return <Square number={i} />
+    }    
 
-    return (
-        <div>
-            <div className="status">{status}</div>
-                <div className="board-row">
-                    {renderSquare(0)}
-                    {renderSquare(1)}
-                    {renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {renderSquare(3)}
-                    {renderSquare(4)}
-                    {renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {renderSquare(6)}
-                    {renderSquare(7)}
-                    {renderSquare(8)}
-                </div>
-        </div>
-    )
+    render() {
+        return (
+            <div>
+                <div className="status">{this.status}</div>
+                    <div className="board-row">
+                        {this.renderSquare(0)}
+                        {this.renderSquare(1)}
+                        {this.renderSquare(2)}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquare(3)}
+                        {this.renderSquare(4)}
+                        {this.renderSquare(5)}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquare(6)}
+                        {this.renderSquare(7)}
+                        {this.renderSquare(8)}
+                    </div>
+            </div>
+        )
+    }
 }
 
 export default Board
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,11 +1,22 @@
 import "./App.css"
+import React from "react"
 
-const Square = (props) => {
-    return (
-        <div>
-            <button className="square">{props.number}</button> 
-        </div>
-    )
+class Square  extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: null
+        }
+            
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={() => this.setState({value: 'X'})} className="square">{this.state.value}</button> 
+            </div>
+        )
+    }
 }
 
 export default Square
